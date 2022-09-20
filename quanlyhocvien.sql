45f8aa4fba65d68dae9b3347ef453b98406dee2a
quanph14 <phquan.0493@gmail.com>
2022-09-20 16:02:28 +0700

    minitest 2009

---
+++ quanlyhocvien.sql
@@ -0,0 +1,5 @@
+use quanlyhocvien;
+select * from students where fullname like '%Nguyen';
+select * from students where fullname like '%Anh';
+select * from students where age >= 15 and age <=18;
+select * from students where id = 12 or id = 13;
